f805a0f96c3f59b33c0c419414f582ce36529a45
hyooojing <gywjd120475@gmail.com>
2026-01-03 22:17:09 +0900

    feat: 카테고리 조회 #5
    - 메인화면 카테고리 불러오기
    - 게시물 카테고리 조회는 추후

--- LogLog_back/src/main/java/com/example/loglog/service/CategoryService.java
+++ LogLog_back/src/main/java/com/example/loglog/service/CategoryService.java
@@ -1,4 +1,22 @@
 package com.example.loglog.service;
 
+import com.example.loglog.entity.Category;
+import com.example.loglog.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public List<Category> getCategories() {
+        return categoryRepository.findAll();
+    }
 }
+
